47450b024314d4b9b4d88e8c8fd0b6c5e5837da2
k_harasawa <k_harasawa@192.168.1.71>
2017-06-28 16:34:51 +0900

    修正

--- practice1/src/jp/co/bananadream/practice1/harasawa/Omikuji.java
+++ practice1/src/jp/co/bananadream/practice1/harasawa/Omikuji.java
@@ -1,28 +1,42 @@
 package jp.co.bananadream.practice1.harasawa;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Omikuji {
 	public static void main(String[] args) {
-	
-	//リストに値を詰める
-	ArrayList<String> kuji = new ArrayList<String>();
-	kuji.add("大吉");
-	kuji.add("中吉");
-	kuji.add("小吉");
-	kuji.add("末吉");
-	kuji.add("吉");
-	kuji.add("凶");
-	
-	//ランダムで数字をだす（０〜５）
-	Random rand = new Random();
-	int num = rand.nextInt(5);
-	
-	//出力
-	System.out.print("今日の運勢は");
-	System.out.print(kuji.get(num));
-	System.out.print("です。");
-	
+
+
+		System.out.println(Omiregist());
+
+	}
+
+	public static String Omiregist(){
+
+		//リストに値を詰める
+		List<String> kuji = new ArrayList<String>();
+		kuji.add("大吉");
+		kuji.add("中吉");
+		kuji.add("小吉");
+		kuji.add("末吉");
+		kuji.add("吉");
+		kuji.add("凶");
+
+		//ランダムで数字をだす（０〜５）
+		Random rand = new Random();
+		int num = rand.nextInt(5);
+
+		//文字列連結
+		String s1 = "今日の運勢は";
+		String s2 = kuji.get(num);
+		String s3 = "です。";
+		StringBuffer buf = new StringBuffer();
+		buf.append(s1);
+		buf.append(s2);
+		buf.append(s3);
+		String s = buf.toString();
+
+		return s;
 	}
 }
