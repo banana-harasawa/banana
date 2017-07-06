3fa45915b52f5adb89e3012e7eadc5b2da03fed3
k_harasawa <k_harasawa@192.168.1.71>
2017-07-06 19:40:08 +0900

    7月6日

---
+++ practice2/src/jp/co/bananadream/practice2/harasawa/DateBean.java
@@ -0,0 +1,16 @@
+package jp.co.bananadream.practice2.harasawa;
+
+
+public class DateBean {
+
+	String today;
+
+	public String getToday() {
+		return today;
+	}
+
+	public void setToday(String today) {
+		this.today = today;
+	}
+
+}
--- practice2/src/jp/co/bananadream/practice2/harasawa/Start.java
+++ practice2/src/jp/co/bananadream/practice2/harasawa/Start.java
@@ -1,164 +1,121 @@
 package jp.co.bananadream.practice2.harasawa;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Random;
 
 public class Start {
 	public static void main(String[]args)throws IOException{
-		try{
-			//ファイルを読み込む
-			File f = new File("/Users/k_harasawa/Documents/uranai.csv");
-			BufferedReader br = new BufferedReader(new FileReader(f));
+		//ReadFileからおみくじを読み込む
+		ReadFile read =new ReadFile();
+		List<Omikuji> al = read.readFile();
 
-			String line;
-			//1行ずつCSVファイルを読み込む
-			//読み込んだデータがnullでなければ
-			while((line = br.readLine()) != null){
-				//行をカンマ区切りで配列に変換
-				String[] data =line.split(",", 0);
+		while(true){
+			//生年月日を入力してください
+			BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("生年月日を入力してください（yyyyMMddの形で入力して下さい）");
 
-				//Listを生成
-				List<Omikuji> al = new ArrayList<Omikuji>();
+			try {
+				//Date型を詰めるBean
+				DateBean today = new DateBean();
 
-				//data[0]の中身が大吉ならば、Daikichiオブジェクトを生成
-				if(data[0].equals("大吉")){
-					Daikichi dai = new Daikichi();
+				//入力
+				String strDate = br2.readLine();
 
-					//data[0]を読み込んで						
-					dai.setUnsei(data[0]);
-					dai.setNegaigoto(data[1]);
-					dai.setAkinai(data[2]);
-					dai.setGakumon(data[3]);
+				//yyyyMMddの形に変換
+				DateFormat df = new SimpleDateFormat("yyyyMMdd");
+				df.setLenient(false); // ←これで厳密にチェックしてくれるようになる
+				String s1 = strDate;
+				String s2 = df.format(df.parse(s1)); // ←df.parseでParseExceptionがThrowされる
 
-					//大吉にセットした値をリストに格納
-					al.add(dai); 
-					String daikichi=dai.disp();
-					System.out.println(daikichi);
+
+
+				//一番新しい今日の日付を生成
+				Date today1 = new Date();
+				//yyyyMMddの形に変換
+				String day1 = df.format(today1);
+				today.setToday(day1);
+				//System.out.println(day);
+
+				//比較するための今日の日付を生成
+				Date today2 =new Date();
+				//yyyyMMddの形に変換
+				String day2 = df.format(today2);
+				//System.out.println(day2);
+
+
+				//入力された生年月日によって結果が変わる（同じ誕生日だと同じ結果を返す）
+				Random r = new Random( Long.parseLong(strDate));
+
+				//today1とtoday2を比較
+				int diff = day2.compareTo(day1);
+				//System.out.println(diff);
+
+				//今日が今日だったら同じ結果を返す
+				if(diff == 0){
+					//前に入力された生年月日と同じ結果を返す
+					System.out.println(al.get(r.nextInt(al.size())).disp());
+
+					//今日が今日ではなかったら新しくランダムを生成しくじを引く
+				}else if(diff>0){
+					int nowDate = Integer.parseInt(df.format(day2)) ;
+					int seed = nowDate - Integer.parseInt(strDate) ;
+					r = new Random(seed);
+					System.out.println(al.get(r.nextInt(al.size())).disp());
+
+				}else{
+					System.out.println("過去");
 				}
 
-				//data[0]が中吉ならば
-				if(data[0].equals("中吉")){
-					//中吉オブジェクトを生成
-					Tyukichi tyu = new Tyukichi();
-
-					//data[0]を読み込んで							
-					tyu.setUnsei(data[0]);
-					tyu.setNegaigoto(data[1]);
-					tyu.setAkinai(data[2]);
-					tyu.setGakumon(data[3]);
-
-					//大吉にセットした値をリストに格納
-					al.add(tyu); 
-					String tyukichi=tyu.disp();
-
-
-				}
-				if(data[0].equals("小吉")){
-					Syoukichi syo = new Syoukichi();
-					//data[0]を読み込んで							
-					syo.setUnsei(data[0]);
-					syo.setNegaigoto(data[1]);
-					syo.setAkinai(data[2]);
-					syo.setGakumon(data[3]);
-
-					//大吉にセットした値をリストに格納
-					al.add(syo); 
-					String syokichi=syo.disp();
-				}
-				if(data[0].equals("吉")){
-					Kichi kichi = new Kichi();
-
-					kichi.setUnsei(data[0]);
-					kichi.setNegaigoto(data[1]);
-					kichi.setAkinai(data[2]);
-					kichi.setGakumon(data[3]);
-
-					//吉にセットした値をリストに格納
-					al.add(kichi); 
-					String kiti=kichi.disp();
-
-				}
-				if(data[0].equals("末吉")){
-					Suekichi sue = new Suekichi();
-					//brが読み込み可能かどうか判定する
-
-					sue.setUnsei(data[0]);
-					sue.setNegaigoto(data[1]);
-					sue.setAkinai(data[2]);
-					sue.setGakumon(data[3]);
-
-					//大吉にセットした値をリストに格納
-					al.add(sue); 
-					String suekichi=sue.disp();
-
-				}
-				if(data[0].equals("凶")){
-					Kyo kyo = new Kyo();
-
-					kyo.setUnsei(data[0]);
-					kyo.setNegaigoto(data[1]);
-					kyo.setAkinai(data[2]);
-					kyo.setGakumon(data[3]);
-
-					//大吉にセットした値をリストに格納
-					al.add(kyo); 
-					String kyou=kyo.disp();
-					System.out.println(kyo);
-				}
-//				for(int i=0; i<50; i++){
-//					System.out.println(al.dai.toString());
-//				}
-
-
-				//誕生日を入力
-				BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
-				System.out.println("生年月日を入力してください");
-				try {
-					String strDate=br2.readLine();
-
-					DateFormat df = DateFormat.getDateInstance();
-					SimpleDateFormat formatter = (SimpleDateFormat)DateFormat.getDateInstance();
-					
-					
-					Date date = formatter.parse(br.readLine());
-					formatter.applyPattern("yyyyMMdd");
-					String b = formatter.format(date);
-					System.out.println(b);
-				}
-				catch (ParseException ex) {
-					System.out.println("解析失敗");
-				}
+			} catch (ParseException p) {
+				p.printStackTrace();
 				
 
-				//ランダムで数字をだす（０〜５）
-				Random rand = new Random();
-				int num = rand.nextInt(5);
-				//数字を使ってリストの中から一つ選び出す
-
-//				//もし入力された誕生日が当日なら
-//				if(strDate == data){
-//					System.out.println(strDate);
-//				}
 			}
-			
-			br.close();
-
-		}catch(IOException e){
-			System.out.println(e);
 		}
-
-
 	}
+	//
+	//				//today1の中身が20170707の場合
+	//				String dateStr = "20170707";
+	//
+	//				//				//一番新しい今日の日付を生成
+	//				//				Date today1 = new Date();
+	//				//				//yyyyMMddの形に変換
+	//				//				SimpleDateFormat sdf1= new SimpleDateFormat("yyyyMMdd");
+	//				//				today.setToday(today1);
+	//
+	////				//比較するための今日の日付を生成
+	////				today2 =new Date();
+	//
+	//				//入力された生年月日によって結果が変わる（同じ誕生日だと同じ結果を返す）
+	//				//r = new Random( Long.parseLong(strDate));
+	//
+	//				//today1とtoday2を比較
+	//				diff = dateStr.compareTo(day2);
+	//				System.out.println(diff);
+	//
+	//				//今日が今日だったら同じ結果を返す
+	//				if(diff == 0){
+	//					//前に入力された生年月日と同じ結果を返す
+	//					System.out.println(al.get(r.nextInt(al.size())).disp());
+	//
+	//					//未来だったら新しくランダムを生成しくじを引く
+	//				}else if(diff>0){
+	//					int nowDate = Integer.parseInt(day2) ;
+	//					int seed = nowDate - Integer.parseInt(strDate) ;
+	//					r = new Random(seed);
+	//					System.out.println(al.get(r.nextInt(al.size())).disp());
+	//
+	//				}else{
+	//					System.out.println("過去");
+	//				}
+
 
 }
 
